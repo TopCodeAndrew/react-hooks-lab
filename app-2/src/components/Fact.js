1bb01c576aa93f5e7263cf406dd1cf244a52a55e
Andrew Smith <topcodeandrew@gmail.com>
2020-12-17 17:01:31 -0700

    app 2 done

---
+++ app-2/src/components/Fact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fact = (props) => {
+    return (
+        <div style={{ marginTop: '30px' }}>
+            {props.text}
+        </div>
+    )
+}
+
+export default Fact
---
+++ app-2/src/components/List.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Fact from './Fact'
+
+const List = () => {
+    const [facts, updateFacts] = useState([])
+    const url = 'https://cat-fact.herokuapp.com/facts'
+
+
+
+    useEffect(() => {
+        axios
+            .get(`${url}`)
+            .then((res) => {
+                updateFacts([...res.data])
+            })
+    }, [])
+
+    const mappedFacts = facts.map((e, i) => {
+        console.log(facts);
+        return (
+            <Fact text={e.text} key={i} />
+        )
+    })
+
+    return (
+        <div>{mappedFacts}</div>
+    )
+}
+
+
+export default List
